22da286fa9dfc8974ee57285fa675c0265150fa4
GuiLana <guilherme.lana@paytrack.com.br>
2021-01-02 16:20:00 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/guilherme/cursomc/services/ClienteService.java
@@ -0,0 +1,22 @@
+package com.guilherme.cursomc.services;
+
+import com.guilherme.cursomc.domain.Cliente;
+import com.guilherme.cursomc.repositories.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ClienteService {
+
+    @Autowired
+    private ClienteRepository repo;
+
+    public Cliente buscar(Integer id){
+        Optional<Cliente> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFountException(
+                "Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()
+        ));
+    }
+}
