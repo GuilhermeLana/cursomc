b2785f139c2b31e94dec4847c3e9486fe8481e8f
GuiLana <guilherme.lana@paytrack.com.br>
2021-01-05 16:48:08 -0300

    PUT, DELETE e GEt para Cliente

---
+++ src/main/java/com/guilherme/cursomc/dto/ClienteDTO.java
@@ -0,0 +1,54 @@
+package com.guilherme.cursomc.dto;
+
+import com.guilherme.cursomc.domain.Cliente;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+public class ClienteDTO {
+
+    private Integer id;
+
+    @NotEmpty(message = "Preenchimento obrigatório!")
+    @Length(min = 5, max = 240, message = "Mínimo de 5 caracteres e máximo de 240!")
+    private String nome;
+
+    @NotEmpty(message = "Preenchimento obrigatório!")
+    @Email(message = "E-mail inválido")
+    private String email;
+
+    public ClienteDTO(){
+
+    }
+
+    public ClienteDTO(Cliente cliente){
+        id = cliente.getId();
+        nome = cliente.getNome();
+        email = cliente.getEmail();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
--- src/main/java/com/guilherme/cursomc/services/ClienteService.java
+++ src/main/java/com/guilherme/cursomc/services/ClienteService.java
@@ -1,23 +1,69 @@
 package com.guilherme.cursomc.services;
 
 import com.guilherme.cursomc.domain.Cliente;
+import com.guilherme.cursomc.domain.Cliente;
+import com.guilherme.cursomc.dto.ClienteDTO;
 import com.guilherme.cursomc.repositories.ClienteRepository;
+import com.guilherme.cursomc.services.exceptions.DataIntegrityException;
 import com.guilherme.cursomc.services.exceptions.ObjectNotFountException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
 public class ClienteService {
 
     @Autowired
-    private ClienteRepository repo;
+    private ClienteRepository clienteRepository;
 
     public Cliente find(Integer id){
-        Optional<Cliente> obj = repo.findById(id);
+        Optional<Cliente> obj = clienteRepository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFountException(
                 "Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()
         ));
+    }
+
+    public Cliente insert(Cliente obj){
+        obj.setId(null);
+        return clienteRepository.save(obj);
+    }
+
+    public Cliente update(Cliente obj){
+        Cliente newCliente = find(obj.getId());
+        updateData(newCliente, obj);
+        return clienteRepository.save(obj);
+    }
+
+    public void delete(Integer id){
+        find(id);
+        try {
+            clienteRepository.deleteById(id);
+        } catch (DataIntegrityViolationException e){
+            throw new DataIntegrityException("Não é possível excluir pois possui entidades relacionadas!");
+        }
+    }
+
+    public List<Cliente> findAll(){
+        return clienteRepository.findAll();
+    }
+
+    public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+        return clienteRepository.findAll(pageRequest);
+    }
+
+    public Cliente fromDTO(ClienteDTO clienteDTO){
+        return new Cliente(clienteDTO.getId(), clienteDTO.getNome(), clienteDTO.getEmail(), null, null);
+    }
+
+    private void updateData(Cliente newCliente, Cliente cliente){
+        newCliente.setNome(cliente.getNome());
+        newCliente.setEmail(cliente.getEmail());
     }
 }
