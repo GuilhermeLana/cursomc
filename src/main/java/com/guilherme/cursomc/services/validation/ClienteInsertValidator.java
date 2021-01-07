4bab52ff3f46b5c984caa9637fdfd22819a65b4f
GuiLana <guilherme.lana@paytrack.com.br>
2021-01-07 14:56:13 -0300

    Validação customizada de duplicação de email

--- src/main/java/com/guilherme/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/guilherme/cursomc/services/validation/ClienteInsertValidator.java
@@ -1,9 +1,12 @@
 package com.guilherme.cursomc.services.validation;
 
+import com.guilherme.cursomc.domain.Cliente;
 import com.guilherme.cursomc.domain.enums.TipoCliente;
 import com.guilherme.cursomc.dto.ClienteNewDTO;
+import com.guilherme.cursomc.repositories.ClienteRepository;
 import com.guilherme.cursomc.resources.exception.FieldMessage;
 import com.guilherme.cursomc.services.validation.utils.BR;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,6 +15,9 @@ import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO>{
+
+    @Autowired
+    private ClienteRepository clienteRepository;
 
     @Override
     public void initialize(ClienteInsert ann){
@@ -28,6 +34,10 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
             list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
         }
 
+        Cliente cliente = clienteRepository.findByEmail(objDto.getEmail());
+        if (cliente != null){
+            list.add(new FieldMessage("email", "E-mail já existente"));
+        }
 
         for(FieldMessage e : list){
             context.disableDefaultConstraintViolation();
